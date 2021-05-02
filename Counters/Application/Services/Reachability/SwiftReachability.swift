7a009f0684d56606362ae16e0eaccf7de420ffa0
Alejandro Melo <almelo@paypal.com>
2021-05-01 21:43:42 -0400

    Adding a disclaimer for SwiftReachability.

--- Counters/Application/Services/Reachability/SwiftReachability.swift
+++ Counters/Application/Services/Reachability/SwiftReachability.swift
@@ -2,7 +2,13 @@ import Foundation
 
 public class SwiftReachability {
 
+    /// We don't need `SwiftReachability` to have a singleton instance,
+    /// but it won't hurt to have the choice. This should be injected
+    /// in the majority of use cases, being classes instantiated by the
+    /// system the only exceptions (like in `AppDelegate`, where we can't
+    /// define the initializer being used).
     static let `default`: SwiftReachability = .init()
+
     private let reachability: Reachability
 
     public init(reachability: Reachability = Reachability.forInternetConnection()) {
